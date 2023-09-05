4019a7e79485638888c1a3355d17f62772a3b7aa
Sean Tang <seantang001@g.ucla.edu>
2023-09-04 22:18:08 -0700

    doing last lab

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -70,14 +70,50 @@ sys_sleep(void)
 }
 
 
-#ifdef LAB_PGTBL
+// Read pgaccess_test() in user/pgtlbtest.c to see how pgaccess is used.
+// Start by implementing sys_pgaccess() in kernel/sysproc.c.
+// You'll need to parse arguments using argaddr() and argint().
+// For the output bitmask, it's easier to store a temporary buffer in the kernel and copy it to the user (via copyout()) after filling it with the right bits.
+// It's okay to set an upper limit on the number of pages that can be scanned.
+// walk() in kernel/vm.c is very useful for finding the right PTEs.
+// You'll need to define PTE_A, the access bit, in kernel/riscv.h. Consult the RISC-V privileged architecture manual to determine its value.
+// Be sure to clear PTE_A after checking if it is set. Otherwise, it won't be possible to determine if the page was accessed since the last time pgaccess() was called (i.e., the bit will be set forever).
+// vmprint() may come in handy to debug page tables.
+
+
+// #ifdef LAB_PGTBL
 int
 sys_pgaccess(void)
 {
-  // lab pgtbl: your code here.
-  return 0;
+  uint64 sa;
+  int pg_num;
+  uint64 res;
+
+  argaddr(0, &sa);
+  argint(1, &pg_num);
+  argaddr(2, &res);
+
+  if (pg_num > 40){
+    return 1;
+  }
+  struct proc* p = myproc();
+  pagetable_t pt = p->pagetable;
+
+  unsigned int abits = 0;
+
+  for (int i=0;i<pg_num;i++){
+    pte_t* pa = walk(pt, sa+i*PGSIZE, 0);
+    if (*pa & (PTE_A)){
+      abits = abits | (1<<i);
+      *pa = *pa & (~PTE_A);
+    }
+  }
+
+  copyout(pt, res, (char*)&abits, sizeof(abits));
+
+  return 0;  
 }
-#endif
+// #endif
 
 uint64
 sys_kill(void)
--- kernel/vm.c
+++ kernel/vm.c
@@ -437,3 +437,37 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+
+void 
+vmprint_helper(pagetable_t pagetable, int lvl){
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X|PTE_U)) == 0){
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      for (int x=0;x<lvl;x++){
+        printf(" ..");
+      }
+      printf("%d: pte %p pa %p\n", i, pte, child);
+
+      vmprint_helper((pagetable_t) child, lvl+1);
+
+    } else if(pte & PTE_V){
+      uint64 child = PTE2PA(pte);
+      for (int x=0;x<lvl;x++){
+        printf(" ..");
+      }
+      printf("%d: pte %p pa %p\n", i, pte, child);
+    }
+  }
+}
+
+// Recursively free page-table pages.
+// All leaf mappings must already have been removed.
+void
+vmprint(pagetable_t pagetable){
+  printf("page table %p\n", pagetable);
+  vmprint_helper(pagetable, 1);
+}
