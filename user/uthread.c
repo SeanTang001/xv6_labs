a021ec8015fdff31b3aa74fa987dd243dd31db72
Sean Tang <seantang001@g.ucla.edu>
2023-10-15 22:12:00 -0700

    finished uthread

--- user/uthread.c
+++ user/uthread.c
@@ -11,13 +11,35 @@
 #define MAX_THREAD  4
 
 
+struct thread_context {
+  uint64 ra;
+  uint64 sp;
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+
+  uint64 last_function;
+  struct thread_context context; 
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+extern void thread_switch(struct thread_context*, struct thread_context*); //
               
 void 
 thread_init(void)
@@ -35,13 +57,13 @@ void
 thread_schedule(void)
 {
   struct thread *t, *next_thread;
-
   /* Find another runnable thread. */
   next_thread = 0;
   t = current_thread + 1;
   for(int i = 0; i < MAX_THREAD; i++){
     if(t >= all_thread + MAX_THREAD)
       t = all_thread;
+    // printf("state: %d\n", t->state);
     if(t->state == RUNNABLE) {
       next_thread = t;
       break;
@@ -62,8 +84,13 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
-  } else
+    printf("switching threads\n");
+    thread_switch(&(t->context), &(next_thread->context));
+
+
+  } else{
     next_thread = 0;
+  }
 }
 
 void 
@@ -74,8 +101,13 @@ thread_create(void (*func)())
   for (t = all_thread; t < all_thread + MAX_THREAD; t++) {
     if (t->state == FREE) break;
   }
-  t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->state = RUNNABLE;
+  t->context.sp = (uint64)&t->stack + (STACK_SIZE - 1);
+  t->context.ra = (uint64) func;
+  return;
+  // uint64 * intlocation = (uint64*) (&(t->stack));
+  // *intlocation = (uint64) func;
 }
 
 void 
